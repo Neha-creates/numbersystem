c85b621778b1ce888ed491dd112537a6b6d87744
Neha-creates <n5h1sh1rm12001@gmail.com>
2024-11-18 17:56:20 +0530

    Code updated for calculation of Max, Min and Sum of primeArray

---
+++ app.js
@@ -0,0 +1,15 @@
+const numbers = [19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30];
+for(let i = 0; i <= numbers.length; i++){
+    let count = 0;
+    let primeArray = [];
+    if(numbers[i] % numbers.values == 0){
+        count++;
+        if(count == 2){
+            primeArray.push(numbers[i]);
+            console.log("Prime Number:", numbers[i]);
+        }
+    }
+}
+primeArray = primeArray.((a,b) => a-b);
+console.log("Minimum Value: ", primeArray[0]);
+console.log("Minimum Value: ", primeArray[-1]);
